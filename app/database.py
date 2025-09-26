ae4d45e32de301a14f52128dc8ec5b98019d99a9
Granth Agarwal <heygranth@gmail.com>
2025-09-26 11:47:54 +0530

    setup alembic and designed models

---
+++ app/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+from os import getenv
+
+
+load_dotenv()
+
+DATABASE_URL = getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
