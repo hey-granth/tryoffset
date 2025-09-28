96a78f2ff3dd93fe381b2a1fc33364d1d657aeaf
Granth Agarwal <heygranth@gmail.com>
2025-09-28 14:49:22 +0530

    set up cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
 from . import models, database, routes
+from fastapi.middleware.cors import CORSMiddleware
 
 
 models.Base.metadata.create_all(bind=database.engine)
 
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "https://tryoffset.onrender.com"
+]
+
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(routes.router)
